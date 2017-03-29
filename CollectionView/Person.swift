e978ad935df02505e18f83318ba7c24358ead69d
luk dro <lukdro@luks-iMac.local>
2017-03-29 05:58:48 -0700

    Tutorial project from "Hackingwithswift" ,CollectionView, loading images from camera roll, Alertsheet, adding name

---
+++ CollectionView/Person.swift
@@ -0,0 +1,21 @@
+//
+//  Person.swift
+//  CollectionView
+//
+//  Created by luk dro on 3/28/17.
+//  Copyright © 2017 luk dro. All rights reserved.
+//
+
+import UIKit
+
+class Person: NSObject {
+    var name: String
+    var image: String
+    
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+        
+    }
+
+}
---
+++ CollectionView/PersonCell.swift
@@ -0,0 +1,15 @@
+//
+//  PersonCell.swift
+//  CollectionView
+//
+//  Created by luk dro on 3/28/17.
+//  Copyright © 2017 luk dro. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+}
